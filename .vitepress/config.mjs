8ea57f030dc1a5c4a3972dea6dee14739af26173
jm3wc <91107886+jm3wc@users.noreply.github.com>
2024-12-22 22:56:40 +0800

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:/vblog2/,
   title: '链知',
   description: 'Just playing around.',
   themeConfig: {
