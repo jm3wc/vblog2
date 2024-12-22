77db73bba12c4fa69e59e8f6e9ab8e36940a3d21
jm3wc <91107886+jm3wc@users.noreply.github.com>
2024-12-22 23:01:03 +0800

    Update index.js

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -5,6 +5,7 @@ import './style.css'
 
 /** @type {import('vitepress').Theme} */
 export default {
+  base:"/vblog2/"
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
